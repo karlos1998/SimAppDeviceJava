c5ef1857f28676583e303d01141a01237eab26de
karlos3098 <fadeusz98@gmail.com>
2023-12-09 22:47:18 +0100

    fix background service

--- app/src/main/java/it/letscode/simappdevice/MyBackgroundService.java
+++ app/src/main/java/it/letscode/simappdevice/MyBackgroundService.java
@@ -1,10 +1,13 @@
 package it.letscode.simappdevice;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.app.Service;
 
 public class MyBackgroundService extends Service {
+
+    private static final long DELAY_TIME = 30000;
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -12,10 +15,23 @@ public class MyBackgroundService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Tutaj umieść kod, który ma być wykonywany w tle
-        // Możesz umieścić kod do wysyłania wiadomości SMS
-        sendSMS();
-        return START_STICKY; // Ta flaga sprawi, że usługa będzie ponownie uruchamiana w razie jej zatrzymania
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sendSMS();
+                startMainActivity();
+            }
+        }, DELAY_TIME);
+
+        return START_STICKY;
+    }
+
+    private void startMainActivity() {
+        Intent mainActivityIntent = new Intent(this, MainActivity.class);
+        mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(mainActivityIntent);
     }
 
     private void sendSMS() {
