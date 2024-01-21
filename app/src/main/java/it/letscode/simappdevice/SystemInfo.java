96844c8d889b5b4430adc0b75f4f353be7fd7182
karlos3098 <fadeusz98@gmail.com>
2024-01-21 18:31:13 +0100

    add operator name, phone number, bts data to system info json

--- app/src/main/java/it/letscode/simappdevice/SystemInfo.java
+++ app/src/main/java/it/letscode/simappdevice/SystemInfo.java
@@ -1,14 +1,64 @@
 package it.letscode.simappdevice;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.telephony.CellIdentityGsm;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import androidx.core.app.ActivityCompat;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 public class SystemInfo {
 
+
+    public JSONArray getBtsJsonData() {
+
+        if (ActivityCompat.checkSelfPermission(ApplicationContextProvider.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+
+        TelephonyManager telephonyManager = (TelephonyManager) ApplicationContextProvider.getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        List<CellInfo> cellInfoList = telephonyManager.getAllCellInfo();
+        JSONArray data = new JSONArray();
+
+        for (CellInfo cellInfo : cellInfoList) {
+            if (cellInfo instanceof CellInfoGsm) {
+                CellIdentityGsm cellIdentity = ((CellInfoGsm) cellInfo).getCellIdentity();
+                int cid = cellIdentity.getCid(); // ID stacji bazowej
+                int lac = cellIdentity.getLac(); // Lokalny kod obszaru
+                Log.d("NetworkInfo", "BTS ID: " + cid + ", LAC: " + lac);
+                data.put(new JSONObject(){{
+                    try {
+                        put("lac", lac);
+                        put("cid", cid);
+                    } catch (JSONException ignore) {}
+                }});
+            }
+        }
+
+        return data;
+    }
     public JSONObject getJsonDetails() {
+
         JSONObject deviceInfo = new JSONObject();
         try {
             deviceInfo.put("manufacturer", getManufacturer());
@@ -19,7 +69,10 @@ public class SystemInfo {
             deviceInfo.put("androidVersion", getAndroidVersion());
             deviceInfo.put("buildId", getBuildId());
             deviceInfo.put("sdkVersion", getSdkVersion());
-            
+            deviceInfo.put("operatorName", getOperatorName());
+            deviceInfo.put("phoneNumber", getPhoneNumber());
+            deviceInfo.put("bts", getBtsJsonData());
+
             Log.d("DeviceInfo", deviceInfo.toString(4));
         } catch (JSONException ignore) {
             Log.d("DeviceInfo", "Json Error!");
@@ -58,6 +111,27 @@ public class SystemInfo {
 
     private int getSdkVersion() {
         return Build.VERSION.SDK_INT;
+    }
+
+    private String getOperatorName() {
+        TelephonyManager telephonyManager = (TelephonyManager) ApplicationContextProvider.getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getNetworkOperatorName();
+    }
+
+    @SuppressLint("HardwareIds")
+    private String getPhoneNumber() {
+        if (ActivityCompat.checkSelfPermission(ApplicationContextProvider.getApplicationContext(), android.Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ApplicationContextProvider.getApplicationContext(), android.Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ApplicationContextProvider.getApplicationContext(), android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "NO_PERMISSION";
+        }
+        TelephonyManager telephonyManager = (TelephonyManager) ApplicationContextProvider.getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getLine1Number();
     }
 
 }
