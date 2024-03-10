51c0b4070957e764398b9e954ff16377f9bbcdf5
karlos1998 <fadeusz98@gmail.com>
2024-03-10 20:59:41 +0100

    enable app full screen

--- app/src/main/java/it/letscode/simappdevice/MainActivity.java
+++ app/src/main/java/it/letscode/simappdevice/MainActivity.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -77,6 +78,13 @@ public class MainActivity extends AppCompatActivity implements ViewManagerListen
 
             setContentView(R.layout.activity_main);
 
+            // full screeen
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
             MyPreferences myPreferences = new MyPreferences();
 
